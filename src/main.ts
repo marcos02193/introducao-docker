b45ee21726cc5634e0168fa2951499a2784ba2cd
marcos willian <mw466169@gmail.com>
2025-09-03 16:56:49 -0300

    :mag:feat: solving container

--- src/main.ts
+++ src/main.ts
@@ -42,6 +42,10 @@ async function bootstrap() {
 
 
     await app.listen(3001);
+    
+// const port = process.env.PORT || 3000;
+// await app.listen(port);
+// console.log(`🚀 Application is running on: http://localhost:${port}`);
 
 }
 bootstrap();
